b13cdc6dc134074b7c9c398dd09f992770a32023
saikatbhuiyan <shaikotbhuiya@gmail.com>
2024-02-28 09:07:47 +0600

    Create Abstract schema and repogitory 1

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,31 @@
+import { FilterQuery, Model, Types } from 'mongoose';
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractDocument } from './abstract.schema';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    return (await createDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model
+      .findOne(filterQuery)
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document was not found');
+    }
+
+    return document;
+  }
+}
