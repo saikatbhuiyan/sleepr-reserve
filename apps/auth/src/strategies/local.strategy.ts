2212459ffb6c161efd72cc0ddcabee15b3c76b89
saikatbhuiyan <shaikotbhuiya@gmail.com>
2024-03-04 09:00:03 +0600

    Auth part 1

--- apps/auth/src/strategies/local.strategy.ts
+++ apps/auth/src/strategies/local.strategy.ts
@@ -1,13 +1,18 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { UsersService } from '../users/users.service';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class LocalStategy extends PassportStrategy(Strategy) {
-  constructor(private readonly userService: UsersService) {
+  constructor(private readonly usersService: UsersService) {
     super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string) {
-    return this.userService.validateUser(email, password);
+    try {
+      return await this.usersService.verifyUser(email, password);
+    } catch (err) {
+      throw new UnauthorizedException(err);
+    }
   }
 }
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,25 +1,25 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly userRepository: UsersRepository) { }
+  constructor(private readonly userRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
-    return this.userRepository.create({ 
-      ...createUserDto, 
-      password: await bcrypt.hash(createUserDto.password, 10) 
+    return this.userRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
     });
   }
 
-  async validateUser(email: string, password: string) {
-    const user = await this.userRepository.findOne({ email })
-    const passwordIsValid = await bcrypt.compare(password, user.password)
+  async verifyUser(email: string, password: string) {
+    const user = await this.userRepository.findOne({ email });
+    const passwordIsValid = await bcrypt.compare(password, user.password);
     if (!passwordIsValid) {
-      throw new UnauthorizedException("Credentials are not valid.")
+      throw new UnauthorizedException('Credentials are not valid.');
     }
-    return user
+    return user;
   }
 }
