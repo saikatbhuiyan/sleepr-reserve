5935399dca51be9b46f2f7246270a4d093a0f7f8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2024-03-04 09:19:16 +0600

    Auth part 2

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,11 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
-import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
