50c98bb5a440da0d21e93486e577ce408e734bc6
Saikat <saikat@Saikats-MacBook-Pro.local>
2024-07-19 22:34:18 +0600

    Üpdate docker files

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,9 +1,8 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { CurrentUser } from '../current-user.decorator';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
-import { UserDocument } from './models/user.schema';
+import { UserDocument, CurrentUser } from '@app/common';
 
 @Controller('users')
 export class UsersController {
